30f2c3f7ea9cd5dbc9d2d11ed4b8cc7e747e6c39
Wellington Faria <wellicfaria@gmail.com>
2020-06-27 21:36:59 -0300

    moduralizando os botoes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,104 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
 
 const url_api = "https://api.hgbrasil.com/finance";
 
-void main() {
+void main() async {
   runApp(MaterialApp(
-      home: Container(
-          child: Text("maria", style: TextStyle(color: Colors.white)))));
+      home: Home(),
+      theme:  ThemeData(
+          hintColor: Colors.amber,
+          primaryColor: Colors.white,
+          inputDecorationTheme: InputDecorationTheme(
+            enabledBorder:
+            OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+            focusedBorder:
+            OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+            hintStyle: TextStyle(color: Colors.amber),
+          ))));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  double dolar;
+  double euro;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$ Conversor \$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: FutureBuilder<Map>(
+        future: getData(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                  child: Text(
+                "CARREGANDO DADOS ...",
+                style: TextStyle(color: Colors.amber, fontSize: 25),
+                textAlign: TextAlign.center,
+              ));
+            default:
+              if (snapshot.hasError) {
+                return Center(
+                    child: Text(
+                  "ERRO EM CARREGAR OS DADOS ...",
+                  style: TextStyle(color: Colors.red, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ));
+              } else {
+                dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+                return SingleChildScrollView(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Icon(Icons.monetization_on,
+                          size: 150, color: Colors.amber),
+                      Divider(),
+                      buildTextField("Reais", "R\$"),
+                      Divider(),
+                      buildTextField("Dolares","US"),
+                      Divider(),
+                      buildTextField("Euros", "€")
+                    ],
+                  ),
+                );
+              }
+          }
+        },
+      ),
+    );
+  }
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(url_api);
+  return json.decode(response.body);
+}
+
+
+Widget buildTextField(String nome_moeda,String simbolo){
+  return TextField(
+    decoration: InputDecoration(
+        labelText: "$nome_moeda",
+        labelStyle: TextStyle(color: Colors.amber),
+        border: OutlineInputBorder(),
+        prefixText: "$simbolo"),
+    style: TextStyle(color: Colors.amber, fontSize: 20),
+  );
 }
